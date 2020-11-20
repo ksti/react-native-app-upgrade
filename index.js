881262fc93e5e480887b6c29ff9f560baaa6a114
GJS <1353990812@qq.com>
2020-11-20 14:20:15 +0800

    js: 一点小优化

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import {
 const RNUpgrade = NativeModules.upgrade;
 const ANDROID_PLATFORM = Platform.OS === 'android';
 
-function handlerVersionString(version) {
+export function handlerVersionString(version) {
     let versions = version.split('.');
     let number = 0;
     if (versions.length === 3) {
@@ -17,6 +17,42 @@ function handlerVersionString(version) {
       number = parseInt(versions[0]) * 10000 + parseInt(versions[1]) * 100
     }
     return number;
+}
+
+// 不考虑字母
+function s2i(s) {
+    return s.split('').reduce(function(a, c) {
+        let code = c.charCodeAt(0);
+        if (48<=code && code < 58) {
+            a.push(code-48);
+        }
+        return a;
+    }, []).reduce(function(a, c) {
+        return 10*a + c;
+    }, 0);
+}
+
+// 1: 大于 0: 等于 -1: 小于
+export function versionCmp(s1, s2) {
+    let a = s1.split('.').map(function(s) {
+        return s2i(s);
+    });
+    let b = s2.split('.').map(function(s) {
+        return s2i(s);
+    });
+    let n = a.length < b.length ? a.length : b.length;
+    for (let i = 0; i < n; i++) {
+        if (a[i] < b[i]) {
+            return -1;
+        } else if (a[i] > b[i]) {
+            return 1;
+        }
+    }
+    if (a.length < b.length) return -1;
+    if (a.length > b.length) return 1;
+    let last1 = s1.charCodeAt(s1.length-1) | 0x20,
+      last2 = s2.charCodeAt(s2.length-1) | 0x20;
+    return last1 > last2 ? 1 : last1 < last2 ? -1 : 0;
 }
 
 /**
@@ -38,7 +74,7 @@ export async function checkUpdate(appId, version) {
                 };
             }
             const msg = res.results[0];
-            if (handlerVersionString(version) < handlerVersionString(msg.version)) {
+            if (versionCmp(version, msg.version) === 1) {
                 return {
                     code: 1,
                     msg: msg.releaseNotes,
